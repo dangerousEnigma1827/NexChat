9c5fd7d315b2768f11ceb8660a0f3508df4982df
Muneeb <syedmuneebmalik1827@gmail.com>
2026-05-24 20:18:37 +0530

    Added side arrow for delete and edit

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -22,6 +22,8 @@ function HomePage() {
     let [onlineUsers, setOnlineUsers] = useState([])
     let [logoutPopupOpen, setLogoutPopupOpen] = useState(false);
     let [dropdownOpen, setDropdownOpen] = useState(false);
+    let [dropArrowdownOpen, setDropArrowdownOpen] = useState(false);
+    let [dropArrowdownId, setDropArrowdownId] = useState(null);
 
     // message schema ke liye
     let [messageType, setMessageText] = useState("text")
@@ -375,12 +377,53 @@ function HomePage() {
                                         {
                                             message.attachments?.length != 0 && (
                                                 message.attachments?.map((attachment, index)=>{
-                                                    return (
-                                                        <div key={index} className={`w-full flex mb-1 ${ message.senderId === currentUserId? "justify-end": "justify-start"}`}>
-                                                            <div className={`max-w-[45%] p-1 ${ message.senderId === currentUserId? "bg-blue-500 mr-4 rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl": "bg-[#1d202f] ml-4 rounded-tr-2xl rounded-br-2xl rounded-bl-2xl"}`}>
+                                                   return (
+                                                        <div 
+                                                            key={index} 
+                                                            className={`w-full flex mb-2 ${
+                                                                message.senderId === currentUserId
+                                                                ? "justify-end"
+                                                                : "justify-start"
+                                                            }`}
+                                                        >
+                                                            <div className={`group relative overflow-visible max-w-[45%] p-1 ${
+                                                                message.senderId === currentUserId
+                                                                ? "bg-blue-500 mr-4 rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl"
+                                                                : "bg-[#1d202f] ml-4 rounded-tr-2xl rounded-br-2xl rounded-bl-2xl"
+                                                            }`}>
+
+                                                                <button 
+                                                                    onClick={() => setDropArrowdownId(attachment.url)}
+                                                                    className={`absolute top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 hover:opacity-100 bg-[#232a3a] rounded-full p-1 transition-all duration-200 z-20 ${
+                                                                        message.senderId === currentUserId
+                                                                        ? "-left-6"
+                                                                        : "-right-6"
+                                                                    }`}>
+                                                                    <svg className="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7"/>
+                                                                    </svg>
+                                                                </button>
+
                                                                 {
-                                                                    attachment.type == "image" && <img src={attachment.url} alt="chat-image" className=' cursor-pointer rounded-xl max-h-[350px] object-cover'/>
+                                                                    dropArrowdownId == attachment.url && (
+                                                                        <div className={`absolute top-1/2 -translate-y-1/2 z-10 bg-[#232a3a] border border-[#31384d] rounded-md divide-y divide-[#31384d] shadow-lg w-44 ${
+                                                                            message.senderId === currentUserId
+                                                                            ? "-left-56"
+                                                                            : "-right-56"
+                                                                        }`}>
+                                                                            <div className="p-2 text-sm text-gray-300 font-medium">
+                                                                                <button className="inline-flex items-center w-full p-2 hover:bg-[#2b3142] hover:text-white rounded transition-all">Delete </button>
+                                                                            </div>
+                                                                        </div>
+                                                                    )
                                                                 }
+
+                                                                {
+                                                                    attachment.type == "image" && (
+                                                                        <img src={attachment.url}  alt="chat-image"  className='cursor-pointer rounded-xl max-h-[350px] object-cover'/>
+                                                                    )
+                                                                }
+
                                                                 <div className='flex justify-end mt-1'>
                                                                     <span className='text-[11px] text-gray-300 font-light'>
                                                                         {
@@ -391,6 +434,7 @@ function HomePage() {
                                                                         }
                                                                     </span>
                                                                 </div>
+
                                                             </div>
                                                         </div>
                                                     )
@@ -400,20 +444,47 @@ function HomePage() {
                                         
                                         { message.text != "" && 
                                             (<div className={`w-full flex mb-1 ${ message.senderId === currentUserId? "justify-end": "justify-start"}`}>
-                                                <div className={`max-w-[45%] px-4 py-3 text-white break-words ${message.senderId === currentUserId? "bg-blue-500 mr-4 rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl": "bg-[#1d202f] ml-4 rounded-tr-2xl rounded-br-2xl rounded-bl-2xl"}`}>
+                                                <div className={`group relative overflow-visible max-w-[45%] px-4 py-3 text-white break-words ${
+                                                    message.senderId === currentUserId ? "bg-blue-500 mr-4 rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl": "bg-[#1d202f] ml-4 rounded-tr-2xl rounded-br-2xl rounded-bl-2xl"}`}>
+
+                                                    <button 
+                                                        onClick={() => setDropArrowdownId(message._id)}
+                                                        className={`absolute top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 hover:opacity-100 bg-[#232a3a] rounded-full p-1 transition-all duration-200 z-20 ${
+                                                            message.senderId === currentUserId
+                                                            ? "-left-6"
+                                                            : "-right-6"
+                                                        }`}>
+                                                        <svg className="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7"/></svg>
+                                                    </button>
+
+                                                    {
+                                                        dropArrowdownId == message._id && (
+                                                            <div className={`absolute top-1/2 -translate-y-1/2 z-10 bg-[#232a3a] border border-[#31384d] rounded-md divide-y divide-[#31384d] shadow-lg w-44 ${
+                                                                message.senderId === currentUserId
+                                                                ? "-left-56"
+                                                                : "-right-56"
+                                                            }`}>
+                                                                <div className="p-2 text-sm text-gray-300 font-medium">
+                                                                    <a href="#" className="inline-flex items-center w-full p-2 hover:bg-[#2b3142] hover:text-white rounded transition-all">Edit</a>
+                                                                    <a href="#" className="inline-flex items-center w-full p-2 hover:bg-[#2b3142] hover:text-white rounded transition-all">Delete</a>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    }
+
                                                     <p className='text-[15px] leading-relaxed'>{message.text}</p>
+
                                                     <div className='flex justify-end mt-1'>
-                                                        {/* time ka part */}
                                                         <span className='text-[11px] text-gray-300 font-light'>
                                                             {
-                                                                new Date(message.createdAt)
-                                                                .toLocaleTimeString([], {
+                                                                new Date(message.createdAt).toLocaleTimeString([], {
                                                                     hour: "2-digit",
                                                                     minute: "2-digit"
                                                                 })
                                                             }
                                                         </span>
                                                     </div>
+
                                                 </div>
                                             </div>)
                                         }
